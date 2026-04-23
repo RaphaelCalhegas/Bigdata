36e873ee566906b9ea5b2e36629081419d97f153
RaphaelCalhegas <calhegasra@cy-tech.fr>
2026-04-24 00:49:47 +0200

    Revert gevent - back to sync worker

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,5 @@
 # gunicorn.conf.py
-timeout         = 120
-workers         = 1
-worker_class    = "gevent"
-worker_connections = 30
-bind            = "0.0.0.0:10000"
+timeout      = 120
+workers      = 1
+bind         = "0.0.0.0:10000"
+worker_class = "sync"
